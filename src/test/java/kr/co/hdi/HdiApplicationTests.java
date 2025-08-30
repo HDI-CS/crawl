7e85cb412fb079c908aa3e8a4eb6c2719aabe1dc
choiseoji <choiseoji123@naver.com>
2025-08-31 00:49:37 +0900

    chore: 프로젝트 생성

---
+++ src/test/java/kr/co/hdi/HdiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.hdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

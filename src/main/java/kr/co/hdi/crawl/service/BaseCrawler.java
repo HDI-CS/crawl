e4c82ce860571e0592bde11be3537f9326c08126
choiseoji <choiseoji123@naver.com>
2025-09-04 19:57:46 +0900

    feat: ChromeDriver 초기 설정 관련 추상 클래스 추가

---
+++ src/main/java/kr/co/hdi/crawl/service/BaseCrawler.java
@@ -0,0 +1,41 @@
+package kr.co.hdi.crawl.service;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.stereotype.Component;
+
+@Component
+public abstract class BaseCrawler {
+
+    protected WebDriver driver;
+
+    protected void initDriver() {
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+
+        // user agent 설정
+        String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36";
+        options.addArguments("--user-agent=" + userAgent);
+        // 창이 뜨지 않도록 headless 모드 설정
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("lang=ko_KR");
+        options.addArguments("--disable-blink-features=AutomationControlled");
+
+        driver = new ChromeDriver(options);
+    }
+
+    protected abstract void crawl();
+
+    public void startCrawling(String webUrl) {
+
+        initDriver();
+        driver.get(webUrl);
+        crawl();
+        driver.quit();
+    }
+}

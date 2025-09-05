86fb70006b065b4f6badb740acc2ce21c6da93ee
최서지 <103797531+choiseoji@users.noreply.github.com>
2025-09-06 01:07:42 +0900

    feat: 올리브영 브랜드 리스트 크롤링 (#8)
    * feat: csv 변환 관련 라이브러리 추가
    
    * feat: 올리브영 브랜드 리스트 크롤링 코드 구현
    
    * feat: ChromeDriver 초기 설정 관련 추상 클래스 추가

--- src/main/java/kr/co/hdi/crawl/controller/CrawlController.java
+++ src/main/java/kr/co/hdi/crawl/controller/CrawlController.java
@@ -2,6 +2,7 @@ package kr.co.hdi.crawl.controller;
 
 import jakarta.websocket.server.PathParam;
 import kr.co.hdi.crawl.service.EnuriCrawlerService;
+import kr.co.hdi.crawl.service.OliveCrawlerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class CrawlController {
 
     private final EnuriCrawlerService enuriCrawlerService;
+    private final OliveCrawlerService oliveCrawlerService;
 
     @PostMapping
     public void crawlProduct(@PathParam("url") String url) {
 
         enuriCrawlerService.startCrawling(url);
+    }
+
+    @PostMapping("/brand")
+    public void getBrandNameInOliveYoung(@PathParam("url") String url) {
+
+        oliveCrawlerService.startCrawling(url);
     }
 }
---
+++ src/main/java/kr/co/hdi/crawl/service/OliveCrawlerService.java
@@ -0,0 +1,82 @@
+package kr.co.hdi.crawl.service;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.springframework.stereotype.Service;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Slf4j
+@Service
+public class OliveCrawlerService extends BaseCrawler {
+
+    @Override
+    protected void crawl() {
+        getBrandName();
+    }
+
+    private void getBrandName() {
+
+        Set<String> brandSet = new HashSet<>();
+        try {
+            Actions actions = new Actions(driver);
+
+            // 강제 스크롤 3번
+            for(int i = 0; i < 3; i++) {
+                actions.sendKeys(Keys.PAGE_DOWN).perform();
+                Thread.sleep(1000);
+            }
+
+            // 뷰티 탭 클릭
+            WebElement beautyButton = driver.findElement(By.xpath("//button[span[text()='뷰티']]"));
+            beautyButton.click();
+            Thread.sleep(1000);
+
+            while (true) {
+                int prevSize = brandSet.size();
+
+                List<WebElement> brandNames = driver.findElements(By.cssSelector(".BrandList_brand-title-wrap___q5zG"));
+                for(WebElement brandName : brandNames) {
+
+                    String kr = brandName.findElement(By.cssSelector("span:nth-of-type(1) > div")).getText();
+                    String eng = brandName.findElement(By.cssSelector("span:nth-of-type(2)")).getText();
+                    brandSet.add(kr + "," + eng);
+                }
+
+                if (brandSet.size() == prevSize) {
+                    break;
+                }
+
+                // 페이지 스크롤
+                actions.sendKeys(Keys.PAGE_DOWN).perform();
+                Thread.sleep(250);
+            }
+        } catch (InterruptedException e) {
+            log.error("크롤링 중 인터럽트 발생", e);
+        } catch (NoSuchElementException e) {
+            log.error("요소를 찾을 수 없음", e);
+        }
+
+        saveBrandsToCSV(brandSet, "/file/path/brands.csv");  // TODO : file path 수정
+    }
+
+    private void saveBrandsToCSV(Set<String>  brandList, String filePath) {
+        try (FileWriter writer = new FileWriter(filePath)) {
+            // 헤더 작성
+            writer.write("한글이름,영문이름\n");
+
+            for(String brand : brandList) {
+                writer.write(brand+"\n");
+            }
+            log.info("CSV 저장 완료: {}", filePath);
+        } catch (IOException e) {
+            log.error("CSV 저장 중 오류 발생", e);
+        }
+    }
+}

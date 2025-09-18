1588779246c6fff1004f9c3d451d7f3d1836d560
choiseoji <choiseoji123@naver.com>
2025-09-18 22:59:46 +0900

    feat: pandarank, kakaka 크롤링 컨트롤러, kakao logo 저장 컨트롤러 추가

--- src/main/java/kr/co/hdi/crawl/controller/CrawlController.java
+++ src/main/java/kr/co/hdi/crawl/controller/CrawlController.java
@@ -1,8 +1,10 @@
 package kr.co.hdi.crawl.controller;
 
 import jakarta.websocket.server.PathParam;
-import kr.co.hdi.crawl.service.EnuriCrawlerService;
-import kr.co.hdi.crawl.service.OliveCrawlerService;
+import kr.co.hdi.crawl.kakao.KakaoCrawlerService;
+import kr.co.hdi.crawl.kakao.KakaoLogoService;
+import kr.co.hdi.crawl.pandarank.PandarankCrawlerService;
+import kr.co.hdi.crawl.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,9 @@ public class CrawlController {
 
     private final EnuriCrawlerService enuriCrawlerService;
     private final OliveCrawlerService oliveCrawlerService;
+    private final KakaoCrawlerService kakaoCrawlerService;
+    private final KakaoLogoService kakaoLogoService;
+    private final PandarankCrawlerService pandarankCrawlerService;
 
     @PostMapping
     public void crawlProduct(@PathParam("url") String url) {
@@ -26,5 +31,23 @@ public class CrawlController {
     public void getBrandNameInOliveYoung(@PathParam("url") String url) {
 
         oliveCrawlerService.startCrawling(url);
+    }
+
+    @PostMapping("/pandarank")
+    public void getPandarankData(@PathParam("url") String url) {
+
+        pandarankCrawlerService.startCrawling(url);
+    }
+
+    @PostMapping("/kakao")
+    public void getBrandList(@PathParam("url") String url) {
+
+        kakaoCrawlerService.startCrawling(url);
+    }
+
+    @PostMapping("/kakao/logo")
+    public void getKakaoLogo() {
+
+        kakaoLogoService.getLogoImage();
     }
 }

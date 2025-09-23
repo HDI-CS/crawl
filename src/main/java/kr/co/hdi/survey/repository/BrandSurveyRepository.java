d7d7024819a7250ab10909de383b6771e104cd86
choiseoji <choiseoji123@naver.com>
2025-09-24 00:38:50 +0900

    feat: BrandSurveyRepository 추가

---
+++ src/main/java/kr/co/hdi/survey/repository/BrandSurveyRepository.java
@@ -0,0 +1,7 @@
+package kr.co.hdi.survey.repository;
+
+import kr.co.hdi.survey.domain.BrandSurvey;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BrandSurveyRepository extends JpaRepository<BrandSurvey, Long> {
+}

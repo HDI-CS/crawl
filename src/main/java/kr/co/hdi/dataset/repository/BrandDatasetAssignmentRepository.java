d199ae83a08247325456e12ec4a325018e803106
choiseoji <choiseoji123@naver.com>
2025-09-24 20:20:00 +0900

    feat: 사용자가 평가해야할 데이터셋 계산하는 jpa 코드

--- src/main/java/kr/co/hdi/dataset/repository/BrandDatasetAssignmentRepository.java
+++ src/main/java/kr/co/hdi/dataset/repository/BrandDatasetAssignmentRepository.java
@@ -1,6 +1,7 @@
 package kr.co.hdi.dataset.repository;
 
 import kr.co.hdi.dataset.domain.BrandDatasetAssignment;
+import kr.co.hdi.user.domain.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,4 +9,6 @@ import java.util.List;
 public interface BrandDatasetAssignmentRepository extends JpaRepository<BrandDatasetAssignment, Long> {
 
     List<BrandDatasetAssignment> findAllByUserId(Long userId);
+
+    long countByUser(UserEntity user);
 }

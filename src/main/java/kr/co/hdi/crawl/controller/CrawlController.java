fa6fde04afbd4b8ed96f249be5c0558230148505
Gothax <scott990328@gmail.com>
2025-09-25 00:14:40 +0900

    fix: api 경로 수정

--- src/main/java/kr/co/hdi/crawl/controller/CrawlController.java
+++ src/main/java/kr/co/hdi/crawl/controller/CrawlController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/crawl")
+@RequestMapping("/v1/crawl")
 public class CrawlController {
 
     private final CrawlService crawlService;
--- src/main/java/kr/co/hdi/survey/controller/SurveyController.java
+++ src/main/java/kr/co/hdi/survey/controller/SurveyController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/survey")
+@RequestMapping("/survey")
 @Slf4j
 @RequiredArgsConstructor
 public class SurveyController {

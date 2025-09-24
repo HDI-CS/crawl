53055653ca9f2bc29bee0743f3d5379c8bbf4c23
choiseoji <choiseoji123@naver.com>
2025-09-24 14:11:40 +0900

    fix: 빌더 public으로 수정

--- src/main/java/kr/co/hdi/dataset/domain/Brand.java
+++ src/main/java/kr/co/hdi/dataset/domain/Brand.java
@@ -1,7 +1,6 @@
 package kr.co.hdi.dataset.domain;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import kr.co.hdi.global.domain.BaseTimeEntityWithDeletion;
@@ -9,9 +8,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import static jakarta.persistence.EnumType.STRING;
 import static jakarta.persistence.GenerationType.IDENTITY;
-import static lombok.AccessLevel.PRIVATE;
 import static lombok.AccessLevel.PROTECTED;
 
 @Entity
@@ -38,8 +35,8 @@ public class Brand extends BaseTimeEntityWithDeletion {
 
     private String image;
 
-    @Builder(access = PRIVATE)
-    private Brand(String brandCode, String brandName, String sectorCategory,
+    @Builder
+    public Brand(String brandCode, String brandName, String sectorCategory,
                   String mainProductCategory, String mainProduct, String target, String referenceUrl, String image) {
 
         this.brandCode = brandCode;
